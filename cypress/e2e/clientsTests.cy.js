6411f73b440f0c0e023ba988d1ceb76982767c68
klimenth <klimenth@bitbucket.securegroup.zone>
2022-10-20 14:25:40 +0300

    visual commits

--- cypress/e2e/clientsTests.cy.js
+++ cypress/e2e/clientsTests.cy.js
@@ -1,8 +1,9 @@
+/// <reference types="cypress" />
+
 describe('Clients tests', () => {
 
-    it("1.[P] Add new CLIENT -> dashboard", () => {
+    beforeEach(() => {
         require('cypress-xpath');
-
         // Default login
         cy.visit('https://inv.bg');
         cy.xpath('//*[@id="inheader"]/a[2]').click();
@@ -14,6 +15,28 @@ describe('Clients tests', () => {
         cy.get('.newFeatureOKbutton').click();
         cy.xpath('//*[@id="logo"]/a/h1').should('have.text', "kikotest");
         cy.xpath("//*[@id='userpanel']/div").should("have.text", 'kikotestsecure@gmail.com');
+    })
+
+    it("1.[P] Add new CLIENT -> dashboard", () => {
+
+        // Click add new client from the dashboard
+        cy.get('[href="https://kikotest.inv.bg/clients/add"]').click();
+        //Select individual
+        cy.get('#type_person').click();
+        //Populate the client information form
+        cy.get('[name="person_name"]').type('OneClient450');
+        cy.get('[name="person_address"]').type('ul. Lozenets 23');
+        cy.get('[name="person_city"]').type('Sofia');
+        //Click submit button
+        cy.get('[name="do_submit"]').click();
+        //Assertion for added message
+        cy.get('#okmsg').should($el => expect($el.text().trim()).to.equal('Клиентът е добавен успешно.'));
+        cy.get(':nth-child(1) > td > .client-section-title').should('have.text', 'Информация за клиента');
+        cy.get(':nth-child(2) > .vertical-align-top > b').should('have.text', 'Име:');
+        cy.get('tbody > :nth-child(2) > :nth-child(2)').should('have.text', 'OneClient450');
+
+    });
+    it.only("1.[P] Try to add existing CLIENT -> dashboard", () => {
 
         // Click add new client from the dashboard
         cy.get('[href="https://kikotest.inv.bg/clients/add"]').click();
@@ -26,10 +49,7 @@ describe('Clients tests', () => {
         //Click submit button
         cy.get('[name="do_submit"]').click();
         //Assertion for added message
-        cy.get('#okmsg').should($el => expect($el.text().trim()).to.equal('Клиентът е добавен успешно.'));
-        cy.get(':nth-child(1) > td > .client-section-title').should('have.text', 'Информация за клиента');
-        cy.get(':nth-child(2) > .vertical-align-top > b').should('have.text', 'Име:');
-        cy.get('tbody > :nth-child(2) > :nth-child(2)').should('have.text', 'OneClient40');
+        cy.get('#error').should($el => expect($el.text().trim()).to.equal('Вече съществува клиент с въведеното име и идентификатор'));
 
     });
 })
