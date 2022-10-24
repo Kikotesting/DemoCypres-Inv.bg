7bbbaf9575589d725d9f71a34e1f46430415eefc
Kikotesting <kikotestsecure@gmail.com>
2022-10-24 20:54:57 +0300

    update small changes

--- cypress/e2e/itemsTests.cy.js
+++ cypress/e2e/itemsTests.cy.js
@@ -1,19 +1,12 @@
 /// <reference types="cypress" />
+import {DefaultPage} from '../pages/defaultPage.js'
 
 describe('Items tests', () => {
     it("1.[P] Add new ITEM -> dashboard", () => {
-        // Click add new item menu
-        cy.get('[href="https://kikotest.inv.bg/objects/add"]').click()
-        // Populate the article information
-        cy.get(':nth-child(1) > [width="170"] > .input--height24px').type('Art3')
-        cy.get(':nth-child(3) > [width="170"] > .input--height24px').type('10')
-        cy.get('div > .input--height24px').clear().type('2')
-        cy.get('.selenium-items-add-form-btn').click()
-        //Assertion
-        cy.get('#okmsg').should($el => expect($el.text().trim()).to.equal('Артикулът е добавен успешно.'))
-        cy.get(':nth-child(1) > .objects-view-left-col > b').should($el => expect($el.text().trim()).to.equal('Име на артикул:'))
-        cy.get(':nth-child(1) > .objects-view-right-col').should($el => expect($el.text().trim()).to.equal('Art3'))
+        const defaultPage = new (DefaultPage)
+        cy.visit('http://inv.bg')
+        //defaultPage.defaultLogIn()
 
-    });
+    })
 
 })
