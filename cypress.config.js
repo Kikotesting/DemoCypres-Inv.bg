3dd8df6da3a9b6eaf72b747ee272c49968433ca0
Kikotesting <kikotestsecure@gmail.com>
2022-10-24 20:11:32 +0300

    disable screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    "video": false
+    "video": false,
+    "screenshots": false
   },
 });
 
