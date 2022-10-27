977b8b2935af72a4fc8a1bc12ab4484fc143cc70
klimenth <klimenth@bitbucket.securegroup.zone>
2022-10-27 14:31:19 +0300

    Push to remote repository

--- cypress/e2e/itemsTests.cy.js
+++ cypress/e2e/itemsTests.cy.js
@@ -2,52 +2,51 @@
 import {DefaultPage} from '../pages/defaultPage.js'
 import {ItemsPage} from '../pages/itemsPage.js'
 import {DashboardPage} from '../pages/dashboardPage.js'
-import { it } from 'mocha'
 
 describe('Positive Items tests', () => {
     beforeEach(() => {
         cy.visit('https://inv.bg')
     })
-    // it("1.[P] Add new ITEM -> dashboard", () => {
-    //     const defaultPage = new (DefaultPage);
-    //     const itemsPage = new (ItemsPage);
-    //     const dashboardPage = new (DashboardPage);
+    it("1.[P] Add new ITEM -> dashboard", () => {
+        const defaultPage = new (DefaultPage);
+        const itemsPage = new (ItemsPage);
+        const dashboardPage = new (DashboardPage);
 
-    //     defaultPage.defaultLogIn()
-    //     dashboardPage.click_addItem()
+        defaultPage.defaultLogIn()
+        dashboardPage.click_addItem()
 
-    //     itemsPage.type_nameItem()
-    //     itemsPage.type_price()
+        itemsPage.type_nameItem()
+        itemsPage.type_price()
 
-    //     itemsPage.clear_quantity()
-    //     itemsPage.type_quantity()
+        itemsPage.clear_quantity()
+        itemsPage.type_quantity()
 
-    //     itemsPage.click_addItemBtn()
+        itemsPage.click_addItemBtn()
 
-    //     // itemsPage.click_popupModalCheckbox()
-    //     // itemsPage.click_popupAddBtn()
+        // itemsPage.click_popupModalCheckbox()
+        // itemsPage.click_popupAddBtn()
 
-    //     itemsPage.assertAddedItemSuccessMessage()
-    //     itemsPage.assertHeaderItemName()
-    // })
+        itemsPage.assertAddedItemSuccessMessage()
+        itemsPage.assertHeaderItemName()
+    })
 
-    // it.only("1.[P] Add existing ITEM -> dashboard", () => {
-    //     const defaultPage = new (DefaultPage);
-    //     const itemsPage = new (ItemsPage);
-    //     const dashboardPage = new (DashboardPage);
+    it.only("1.[P] Add existing ITEM -> dashboard", () => {
+        const defaultPage = new (DefaultPage);
+        const itemsPage = new (ItemsPage);
+        const dashboardPage = new (DashboardPage);
 
-    //     defaultPage.defaultLogIn()
-    //     dashboardPage.click_addItem()
+        defaultPage.defaultLogIn()
+        dashboardPage.click_addItem()
 
-    //     itemsPage.type_nameItem()
-    //     itemsPage.type_price()
+        itemsPage.type_nameItem()
+        itemsPage.type_price()
 
-    //     itemsPage.clear_quantity()
-    //     itemsPage.type_quantity()
+        itemsPage.clear_quantity()
+        itemsPage.type_quantity()
 
-    //     itemsPage.click_addItemBtn()
+        itemsPage.click_addItemBtn()
 
-    //     itemsPage.assertExistingProductErrorMessage()
-    // })
-    it
+        itemsPage.assertExistingProductErrorMessage()
+    })
+
 })
