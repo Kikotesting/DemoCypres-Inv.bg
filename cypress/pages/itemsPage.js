9e03edf0df3d4ec0c766edaf908b9d8a9a93765f
klimenth <klimenth@bitbucket.securegroup.zone>
2022-10-27 14:30:55 +0300

    Update the code with more stable methods

--- cypress/pages/itemsPage.js
+++ cypress/pages/itemsPage.js
@@ -6,28 +6,43 @@ export class ItemsPage {
         quantity : () => cy.get('div > .input--height24px'),
         addItemBtn : () => cy.get('.selenium-items-add-form-btn'),
         successefullyMessage : () => cy.get('#okmsg'),
-        headerItemName : () => cy.get('#headline2 > h2')
+        errorMessageExistingProduct : () => cy.get('#whiterow2 > :nth-child(3)'),
+        headerItemName : () => cy.get('#headline2 > h2'),
+        popupModalCheckbox : () => cy.get('#disable-popup-modal'),
+        popupAddBtn : () => cy.get('#popup-modal'),
     }
 
     // Adding product
     type_nameItem(){
-        this.elements.nameItem().type('Oficer')
+        this.elements.nameItem().type('Oficer5')
     }
     type_price(){
-        this.elements.price().type('10')
+        this.elements.price().type('102')
     }
     type_quantity(){
-        this.elements.customerSubdomainInput().type('1')
+        this.elements.quantity().type('123')
+    }
+    clear_quantity(){
+        this.elements.quantity().clear()
     }
     click_addItemBtn(){
         this.elements.addItemBtn().click()
+    }
+    click_popupModalCheckbox(){
+        this.elements.popupModalCheckbox().click()
+    }
+    click_popupAddBtn(){
+        this.elements.popupAddBtn().click()
     }
     //Assertions
     assertAddedItemSuccessMessage(){
        this.elements.successefullyMessage().should($el => expect($el.text().trim()).to.equal('Артикулът е добавен успешно.'))
     }
+    assertExistingProductErrorMessage(){
+        this.elements.errorMessageExistingProduct().should($el => expect($el.text().trim()).to.equal('Вече сте добавили артикул с това име.'))
+     }
     assertHeaderItemName(){
-        this.elements.headerItemName().should('have.text','Oficer')
+        this.elements.headerItemName().should('have.text','Oficer5')
      }
 
 
